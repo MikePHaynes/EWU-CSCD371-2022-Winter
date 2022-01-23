96d8bf540790c632d29d1ddbbf405f69cb3e3501
Mike <imdukie@gmail.com>
2022-01-22 19:45:40 -0800

    Added "FileLogger" class.

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        public override void Log(LogLevel logLevel, string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
