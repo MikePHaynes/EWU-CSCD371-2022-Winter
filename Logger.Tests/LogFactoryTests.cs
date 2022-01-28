b96b8fe601a65152ae12d92b9ce35afe353e3b62
Mike <imdukie@gmail.com>
2022-01-27 19:20:05 -0800

    Added unit tests for LogFactory, all tests passing.

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,24 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
+        [TestMethod]
+        public void CreateLogger_FileLoggerNotConfigured_ReturnsNull()
+        {
+            LogFactory logFactory = new();
+            var fileLogger = logFactory.CreateLogger(nameof(LogFactoryTests));
+
+            Assert.IsNull(fileLogger);
+        }
+
+        [TestMethod]
+        public void CreateLogger_FileLoggerConfigured_ReturnsFileLogger()
+        {
+            LogFactory logFactory = new();
+            logFactory.ConfigureFileLogger("testfile.txt");
+            var fileLogger = logFactory.CreateLogger(nameof(LogFactoryTests));
+
+            Assert.IsNotNull(fileLogger);
+        }
         
     }
 }
