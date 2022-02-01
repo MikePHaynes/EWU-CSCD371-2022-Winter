af7db19bff3de9135322a795d8b250de09c65675
Mike <imdukie@gmail.com>
2022-01-31 23:30:23 -0800

    Updated Jester class and Main method. Removed arguments parameter in main to get rid of message from compiler.

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -2,16 +2,20 @@
 {
     public class Jester
     {
-        public Jester()
-        {
+        private JokeOutput _jokeOutput;
+        private JokeService _jokeService;
 
+        public Jester(JokeOutput jokeOutput, JokeService jokeService)
+        {
+            _jokeOutput = jokeOutput;
+            _jokeService = jokeService;
         }
 
         public void TellJoke()
         {
-            JokeService jokeService = new();
-            string joke = jokeService.GetJoke();
-            while (joke.Contains("Chuck Norris")) joke = jokeService.GetJoke();
+            string joke = _jokeService.GetJoke();
+            while (joke.Contains("Chuck Norris")) joke = _jokeService.GetJoke();
+            _jokeOutput.PrintJoke(joke);
         }
     }
 }
