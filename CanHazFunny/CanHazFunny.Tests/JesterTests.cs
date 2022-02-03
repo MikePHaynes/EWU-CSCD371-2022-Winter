4eae2c784f52e08a3edf4ef12eed78617c688432
Mike <imdukie@gmail.com>
2022-02-03 14:06:52 -0800

    Implemented first tests for Jester class.

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Jester_OutputDependencyNull_ThrowsException()
+        {
+            new Jester(new JokeOutput(), null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Jester_ServiceDependencyNull_ThrowsException()
+        {
+            new Jester(null, new JokeService());
+        }
         
     }
 }
