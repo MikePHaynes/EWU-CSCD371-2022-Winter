5f36d58b7745729de7532848b47be98611658002
Mike <imdukie@gmail.com>
2022-02-03 13:39:11 -0800

    Jester now takes in and null checks the interfaces, jokes can no longer contain "Norris".

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,20 +1,22 @@
-﻿namespace CanHazFunny
+﻿using System;
+
+namespace CanHazFunny
 {
     public class Jester
     {
-        private JokeOutput _jokeOutput;
-        private JokeService _jokeService;
+        private readonly IJokeOutput _jokeOutput;
+        private readonly IJokeService _jokeService;
 
-        public Jester(JokeOutput jokeOutput, JokeService jokeService)
+        public Jester(IJokeOutput? jokeOutput, IJokeService? jokeService)
         {
-            _jokeOutput = jokeOutput;
-            _jokeService = jokeService;
+            _jokeOutput = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
+            _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
         }
 
         public void TellJoke()
         {
             string joke = _jokeService.GetJoke();
-            while (joke.Contains("Chuck Norris")) joke = _jokeService.GetJoke();
+            while (joke.Contains("Chuck") || joke.Contains("Norris")) joke = _jokeService.GetJoke();
             _jokeOutput.PrintJoke(joke);
         }
     }
