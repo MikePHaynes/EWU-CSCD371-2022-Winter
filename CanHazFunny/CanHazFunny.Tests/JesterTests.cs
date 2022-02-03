0d2786dc6e460ecab36a8daef1f824465c6875ff
Mike <imdukie@gmail.com>
2022-02-03 15:08:23 -0800

    Written unit tests for Jester class.

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace CanHazFunny.Tests
 {
@@ -8,17 +9,37 @@ namespace CanHazFunny.Tests
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Jester_OutputDependencyNull_ThrowsException()
+        public void Constructor_OutputDependencyNull_ThrowsException()
         {
             new Jester(new JokeOutput(), null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Jester_ServiceDependencyNull_ThrowsException()
+        public void Constructor_ServiceDependencyNull_ThrowsException()
         {
             new Jester(null, new JokeService());
         }
-        
+
+        [TestMethod]
+        public void TellJoke_RetrievesFromJokeService_TellsJokeNotNullOrEmpty()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            new Jester(new JokeOutput(), new JokeService()).TellJoke();
+
+            Assert.IsNotNull(stringWriter.ToString());
+            Assert.AreNotEqual<string>("", stringWriter.ToString());
+        }
+
+        [TestMethod]
+        public void TellJoke_RetrievesFromJokeService_TellsJokeNotChuckNorris()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+            new Jester(new JokeOutput(), new JokeService()).TellJoke();
+
+            Assert.IsFalse(stringWriter.ToString().Contains("Chuck") || stringWriter.ToString().Contains("Norris"));
+        }
     }
 }
