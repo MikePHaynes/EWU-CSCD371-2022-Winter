f2e8438070610ca24186019cc1bb909492b70ff3
Benjamin Michaelis <benjamin@michaelis.net>
2022-02-15 17:08:35 -0800

    Add students assignment

---
+++ Assignment/Assignment/SampleData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+    public class SampleData : ISampleData
+    {
+        // 1.
+        public IEnumerable<string> CsvRows
+        {
+            get
+            {
+                FileStream fileStream = File.OpenRead("People.csv");
+                StreamReader reader = new StreamReader(fileStream);
+                List<string> list = new();
+                while(!reader.EndOfStream)
+                {
+                    string? line;
+                    if((line = reader.ReadLine()) != null)
+                        list.Add(line);
+                }
+
+                return list;
+            }
+        }
+
+        // 2.
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
+            => throw new NotImplementedException();
+
+        // 3.
+        public string GetAggregateSortedListOfStatesUsingCsvRows()
+            => throw new NotImplementedException();
+
+        // 4.
+        public IEnumerable<IPerson> People => throw new NotImplementedException();
+
+        // 5.
+        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
+            Predicate<string> filter) => throw new NotImplementedException();
+
+        // 6.
+        public string GetAggregateListOfStatesGivenPeopleCollection(
+            IEnumerable<IPerson> people) => throw new NotImplementedException();
+    }
+}
